0b6d00c0bd2fcd41c1642733c3308c7a060c3257
suelytohm <suelytohm.02@gmail.com>
2022-10-11 00:28:01 -0300

    furst commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Card } from "./components/Card";
+const axios = require("axios").default;
+
+function App() {
+  const [crypto, setCrypto] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.coincap.io/v2/assets")
+      .then(function (response) {
+        // handle success
+        setCrypto(response.data.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
+
+  const financial = (x) => {
+    return Number.parseFloat(x).toFixed(2);
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Top Cryptos</h1>
+        <div className="criptos">
+          {crypto.slice(0, 30).map((currency, key) => (
+            <Card
+              key={key}
+              name={currency.name}
+              symbol={currency.symbol}
+              priceUsd={financial(currency.priceUsd)}
+              changePercent24Hr={financial(currency.changePercent24Hr)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card/index.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import "./Card.css";
+
+import Up from "../../assets/images/up.png";
+import Down from "../../assets/images/down.png";
+
+export const Card = ({ name, symbol, priceUsd, changePercent24Hr }) => {
+  const [corCard, setCorCard] = useState("");
+
+  useEffect(() => {
+    if (changePercent24Hr <= 0) {
+      setCorCard("red");
+    } else {
+      setCorCard("green");
+    }
+  }, [changePercent24Hr]);
+
+  return (
+    <div className={`card ` + corCard}>
+      <h2>
+        {name} ({symbol})
+      </h2>
+      <p>Preço(USD): ${priceUsd}</p>
+      <p>Var(24h): {changePercent24Hr}</p>
+      <img src={corCard === "red" ? Down : Up} alt="" />
+    </div>
+  );
+};
